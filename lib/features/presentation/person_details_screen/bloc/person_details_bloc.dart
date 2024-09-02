6fcdf9f3b5670db89af3ad8638673b5fd80725a1
Josef Guillen <josefguillen1996@gmail.com>
2024-09-02 22:35:51 +0800

    remove unused codes

--- lib/features/presentation/person_details_screen/bloc/person_details_bloc.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutterexamapp/features/presentation/person_details_screen/bloc/person_details_state.dart';
-
-class PersonDetailsBloc extends Cubit<PersonDetailsState> {
-  PersonDetailsBloc() : super(const PersonDetailsState());
-}
--- lib/features/presentation/person_details_screen/bloc/person_details_state.dart
+++
@@ -1,8 +0,0 @@
-import 'package:equatable/equatable.dart';
-
-class PersonDetailsState extends Equatable {
-  const PersonDetailsState();
-
-  @override
-  List<Object?> get props => [];
-}
