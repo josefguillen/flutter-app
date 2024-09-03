97ca7b980e28fd6d286aac0f652b94d72201f7be
Josef Guillen <josefguillen1996@gmail.com>
2024-09-03 13:15:11 +0800

    change the scaffold colors.

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+const colorPrimary = Color(0xFF8E2DE2);
+const colorSecondary = Color(0xFF4A00E0);
+
+const contentBackgroundPrimary = Color(0xFFFAFAFA);
+const contentBackgroundSecondary = Color(0xFFE0E0E0);
+
+const imagePlaceHolderColor = Color(0xFFD1C4E9);
--- lib/features/presentation/widgets/app_scaffold.dart
+++ lib/features/presentation/widgets/app_scaffold.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutterexamapp/core/constants/colors.dart';
 import 'package:go_router/go_router.dart';
 
 class AppScaffold extends StatelessWidget {
@@ -29,18 +31,34 @@ class AppScaffold extends StatelessWidget {
       },
       child: Scaffold(
         appBar: AppBar(
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  colorPrimary,
+                  colorSecondary,
+                ],
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+              ),
+            ),
+          ),
           title: Text(
             title,
-            style: const TextStyle(
-              fontWeight: FontWeight.w500,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              fontSize: 15.h,
             ),
           ),
           centerTitle: true,
           leading: Visibility(
             visible: showBackButton,
             child: IconButton(
-              icon: const Icon(
+              icon: Icon(
                 Icons.arrow_back_ios_new,
+                size: 20.h,
+                color: Colors.white,
               ),
               onPressed: () {
                 context.pop();
@@ -50,20 +68,39 @@ class AppScaffold extends StatelessWidget {
           actions: [
             Visibility(
               visible: showRefreshButton,
+              child: Container(
+                margin: EdgeInsets.only(right: 5.w),
                 child: IconButton(
-                  icon: const Icon(
+                  icon: Icon(
                     Icons.refresh,
+                    size: 25.h,
+                    color: Colors.white,
                   ),
                   onPressed: () {
                     if (onRefreshPress != null) {
                       onRefreshPress!();
                     }
                   },
-                )
+                ),
+              ),
             ),
           ],
         ),
-        body: SizedBox(width: containerWidth, height: containerHeight, child: body),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                contentBackgroundPrimary,
+                contentBackgroundSecondary,
+              ],
+            ),
+          ),
+          width: containerWidth,
+          height: containerHeight,
+          child: body,
+        ),
       ),
     );
   }
